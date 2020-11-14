5faa55651de5aa66333e631dcf8e9e11232b2204
FSWL <fswl98@gmail.com>
2020-11-14 20:13:53 +0300

    Updates

---
+++ precache-manifest.2e096db75dfb4d3447349fe78b25d0a6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a503d4d490045002b054be118a1cab6",
+    "url": "/scientist/index.html"
+  },
+  {
+    "revision": "7d76c394380d762e0303",
+    "url": "/scientist/static/css/main.f5a77890.chunk.css"
+  },
+  {
+    "revision": "18fc5dfeaf046f168b85",
+    "url": "/scientist/static/js/2.a6f6942d.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/scientist/static/js/2.a6f6942d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d76c394380d762e0303",
+    "url": "/scientist/static/js/main.98d3a3ac.chunk.js"
+  },
+  {
+    "revision": "9c0787f46b578d88f60b",
+    "url": "/scientist/static/js/runtime-main.0a34c5ba.js"
+  },
+  {
+    "revision": "8a66c1e9f7422d89a7aac90a29332f94",
+    "url": "/scientist/static/media/Logo.8a66c1e9.svg"
+  },
+  {
+    "revision": "6524822eca65f5765557bfc70e6ff322",
+    "url": "/scientist/static/media/expertise.6524822e.svg"
+  },
+  {
+    "revision": "36010dcfd518f222dff5e765ad8158f1",
+    "url": "/scientist/static/media/location.36010dcf.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scientist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bc60174029d7a7c50c5a7cd0de4cadd2",
+    "url": "/scientist/static/media/map.bc601740.png"
+  }
+]);
