5e38fd61019df4c6459b41128b05525161649c0e
FSWL <fswl98@gmail.com>
2020-11-14 20:39:07 +0300

    Updates

---
+++ precache-manifest.6a779fa5e09b49d7fca75f11642166a1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426a463386e04ef16f193f69fd93cdfa",
+    "url": "/scientists/index.html"
+  },
+  {
+    "revision": "07b8008e2f8557b8ceb2",
+    "url": "/scientists/static/css/main.ca044207.chunk.css"
+  },
+  {
+    "revision": "07a053b0e5d1ad8acdfc",
+    "url": "/scientists/static/js/2.1df24b32.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/scientists/static/js/2.1df24b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b8008e2f8557b8ceb2",
+    "url": "/scientists/static/js/main.79d909b8.chunk.js"
+  },
+  {
+    "revision": "a1e6165a7dddf1bc9ccc",
+    "url": "/scientists/static/js/runtime-main.4b2e006b.js"
+  },
+  {
+    "revision": "8a66c1e9f7422d89a7aac90a29332f94",
+    "url": "/scientists/static/media/Logo.8a66c1e9.svg"
+  },
+  {
+    "revision": "6524822eca65f5765557bfc70e6ff322",
+    "url": "/scientists/static/media/expertise.6524822e.svg"
+  },
+  {
+    "revision": "36010dcfd518f222dff5e765ad8158f1",
+    "url": "/scientists/static/media/location.36010dcf.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scientists/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bc60174029d7a7c50c5a7cd0de4cadd2",
+    "url": "/scientists/static/media/map.bc601740.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scientist/precache-manifest.94c1473db676a595b67e259f9beab0cd.js"
+  "/scientists/precache-manifest.6a779fa5e09b49d7fca75f11642166a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scientist/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scientists/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
