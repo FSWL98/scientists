6145f8f462b17fcc6f86f543f1a636488430ca72
FSWL <fswl98@gmail.com>
2020-11-14 20:29:55 +0300

    added redirect

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import {MainPage} from "./containers/MainPage";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
 import {NewsItemPage} from "./containers/NewsItemPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/scientists" component={MainPage} />
+        <Route exact path="/scientists/main" component={MainPage} />
         <Route exact path="/scientists/cabinet" component={MainPage} />
         <Route exact path="/scientists/news/:newsID" component={NewsItemPage} />
+        <Redirect from='/scientists' to="/scientists/main" />
       </Switch>
     </BrowserRouter>
   );
