8142438fddaa3e35d6085d5d36ef32c574d5d739
FSWL <fswl98@gmail.com>
2021-02-21 20:13:37 +0300

    added docker files

--- run.sh
+++ run.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
 name=front
+user=user
+build=$(pwd)/build
+build_container_name=front_build
 
-npm install
-npm run build
+rm -rf build || true
+mkdir -p build
+
+docker image remove front_build || true
+docker image remove $name || true
+
+docker volume remove front_build || true
+docker volume create --driver local \
+	--opt type=none \
+	--opt device=$build \
+	--opt o=bind front_build
+
+docker build -t $build_container_name -f Dockerfile_create_build .
+docker run -d --rm --name front_build -v front_build:/app/build $build_container_name
+
 docker stop $name || true
 docker rm $name || true 
 
-docker build -t $name .
+docker build -t $name -f Dockerfile .
 
 docker run -d --name $name -p 5000:80 $name
 
+chown -R $user: $build
